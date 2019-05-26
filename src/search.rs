c322fcb4e16aa0ce33ece1e90dc99e34aa725dba
Andrew Halle <ahalle@berkeley.edu>
2019-05-26 13:29:11 -0700

    successful corners solve

--- src/search.rs
+++ src/search.rs
@@ -1,5 +1,24 @@
-use crate::cube::Cube;
 use std::collections::{HashMap, HashSet, VecDeque};
+use std::fs::File;
+use std::path::Path;
+
+use flate2::read::GzDecoder;
+use lazy_static::lazy_static;
+
+use crate::cube;
+use crate::cube::Cube;
+
+lazy_static! {
+    static ref CORNERS_TABLE: Vec<u8> = load_corners_table();
+}
+
+fn load_corners_table() -> Vec<u8> {
+    let path = Path::new("tables/corners.data.gz");
+    let file = File::open(&path).unwrap();
+    let d = GzDecoder::new(file);
+
+    bincode::deserialize_from(d).unwrap()
+}
 
 struct SearchNode<'a> {
     state: Cube<'a>,
@@ -32,19 +51,19 @@ impl<'a> SearchNode<'a> {
 }
 
 impl<'a> SearchNodeSmall<'a> {
-    fn neighbors(&self) -> VecDeque<SearchNodeSmall<'a>> {
+    fn neighbors(&self) -> VecDeque<(SearchNodeSmall<'a>, String)> {
         let mut result = VecDeque::new();
         let all_moves = vec![
             "U", "U'", "U2", "F", "F'", "F2", "R", "R'", "R2", "D", "D'", "D2", "B", "B'", "B2",
             "L", "L'", "L2",
         ];
 
-        for m in all_moves.iter() {
+        for m in all_moves.into_iter() {
             let mut state = self.state.clone();
             state.twist(m);
 
             let distance = self.distance + 1;
-            result.push_back(SearchNodeSmall { state, distance });
+            result.push_back((SearchNodeSmall { state, distance }, m.to_string()));
         }
 
         result
@@ -110,7 +129,7 @@ pub fn gen_table<F: Fn(&Cube) -> usize>(c: Cube, result_size: usize, index_fn: F
 
     while let Some(curr) = queue.pop_front() {
         let neighbors = curr.neighbors();
-        for neighbor in neighbors.into_iter() {
+        for (neighbor, _) in neighbors.into_iter() {
             if solution_table[index_fn(&neighbor.state)] == 0 {
                 solution_table[index_fn(&neighbor.state)] = curr.distance + 1;
                 queue.push_back(neighbor);
@@ -123,7 +142,39 @@ pub fn gen_table<F: Fn(&Cube) -> usize>(c: Cube, result_size: usize, index_fn: F
         }
     }
 
+    solution_table[0] = 0;
+
     solution_table
+}
+
+pub fn solve_corners(c: &Cube) -> String {
+    let mut curr = SearchNodeSmall {
+        state: c.clone(),
+        distance: CORNERS_TABLE[cube::corners_index(c)],
+    };
+    dbg!();
+
+    let mut sol = String::new();
+    while CORNERS_TABLE[cube::corners_index(&curr.state)] != 0 {
+        let neighbors = curr.neighbors();
+        let mut min_distance = 100;
+        let mut next_move = String::new();
+
+        for (neighbor, neighbor_move) in neighbors.into_iter() {
+            let neighbor_distance = CORNERS_TABLE[cube::corners_index(&neighbor.state)];
+            if neighbor_distance < min_distance {
+                min_distance = neighbor_distance;
+                next_move = neighbor_move;
+                curr = neighbor;
+            }
+        }
+
+        sol.push_str(&next_move);
+        sol.push_str(" ");
+        dbg!(&sol);
+    }
+
+    sol.trim().to_string()
 }
 
 #[cfg(test)]
@@ -138,5 +189,13 @@ mod tests {
         let mut c = Cube::new(2, &t);
         c.twist("B' R D2 U2 R' L U D' F2 D' F2 L F2 L2 B");
         println!("{}", search::bfs(c).unwrap());
+    }
+
+    #[test]
+    fn test_solve_corners() {
+        let t = transformations::cube3();
+        let mut c = Cube::new(3, &t);
+        c.twist("B' R D2 U2 R' L U D' F2 D' F2 L F2 L2 B");
+        println!("{}", search::solve_corners(&c));
     }
 }
